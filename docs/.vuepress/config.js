55d66c88efa83f0a86c86d1ce6ecdee3ee26ddd6
Big_Cake <2577117205@qq.com>
2022-07-07 12:12:04 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,18 +3,20 @@ module.exports = {
   description: '社区维护的 PCL 群组服帮助文档 兼 脑溢血治疗中心',
   themeConfig: {
     logo: 'favicon.png',
+    editLinks: true,
+    docsBranch: 'source',
     sidebar: 
          '/pcls/': [
-                ['/pcls/', '首页'],
-                '/pcls/introducing.html',
-                '/pcls/how-to-play.html',
-                '/pcls/q&a.html'
+             ['/pcls/', '首页'],
+             '/pcls/introducing.html',
+             '/pcls/how-to-play.html',
+             '/pcls/q&a.html'
          ],
          '/rules/': [
-                ['/rules/', '首页'],
-                '/rules/qq-group.html',
-                '/rules/pcls.html',
-                '/rules/ask-questions.html'
+             ['/rules/', '首页'],
+             '/rules/qq-group.html',
+             '/rules/pcls.html',
+             '/rules/ask-questions.html'
          ],
     nav: [
       { text: '主页', link: '/' },
