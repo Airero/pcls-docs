e6a801501a6a16405af55456479fb75cd8b1c1a5
Big_Cake <2577117205@qq.com>
2022-07-07 11:53:32 +0800

    Create config.js

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: ' PCL 群组服帮助文档',
+  description: '社区维护的 PCL 群组服帮助文档 兼 脑溢血治疗中心',
+  themeConfig: {
+    logo: 'favicon.png',
+    sidebar: auto,
+    nav: [
+      { text: '主页', link: '/' },
+      { text: '生存服', link: '/pcls/' },
+      { text: '服务器规则', link: '/rules/' }
+      { text: '赞助', link: 'https://pay.z0z0r4.top' target:'_blank' }
+      { text: '封禁列表', link: 'https://bans.z0z0r4.top' target:'_blank' }
+      { text: 'GitHub', link: 'https://github.com/Big-Cake-jpg/pcls-docs' target:'_blank' },
+    ]
+  },
+  head: [
+    ['link', { rel: 'icon', href: 'favicon.png' }]
+  ]
+}
