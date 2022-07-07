d90ebba554c084c870e98d7a423f0bf762219afa
Big_Cake <2577117205@qq.com>
2022-07-07 12:06:08 +0800

    fix sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,19 @@ module.exports = {
   description: '社区维护的 PCL 群组服帮助文档 兼 脑溢血治疗中心',
   themeConfig: {
     logo: 'favicon.png',
-    sidebar: auto,
+    sidebar: 
+         '/pcls/': [
+                ['/pcls/', '首页'],
+                '/pcls/introducing.html',
+                '/pcls/how-to-play.html',
+                '/pcls/q&a.html'
+         ],
+         '/rules/': [
+                ['/rules/', '首页'],
+                '/rules/qq-group.html',
+                '/rules/pcls.html',
+                '/rules/ask-questions.html'
+         ],
     nav: [
       { text: '主页', link: '/' },
       { text: '生存服', link: '/pcls/' },
